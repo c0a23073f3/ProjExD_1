807035e4a8a4804ee80a6bb3978f6ed31fc488a1
taisei <c0a23073@edu.teu.ac.jp>
2024-11-19 17:12:27 +0900

    演習課題　ミス修正

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -9,17 +9,14 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock = pg.time.Clock()
 
-    # 背景画像の読み込み
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_width = bg_img.get_width()
 
-    # こうかとん画像の読み込みと反転
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_rct = kk_img.get_rect()
     kk_rct.center = (300, 200)
 
-    # 背景の初期位置
     bg_x = 0
 
     while True:
@@ -27,14 +24,12 @@ def main():
             if event.type == pg.QUIT: 
                 return
 
-        # 背景を常に左に流す
         bg_x = (bg_x - 1) % bg_width
 
-        # 押下キーの取得
         key_lst = pg.key.get_pressed()
-        move_x, move_y = 0, 0  # 移動量を初期化
+        move_x, move_y = 0, 0  
 
-        # 矢印キーに応じて移動量を設定
+
         if key_lst[pg.K_UP]:
             move_y -= 1
         if key_lst[pg.K_DOWN]:
@@ -48,15 +43,13 @@ def main():
 
         kk_rct.move_ip(move_x, move_y)
 
-        screen.blit(bg_img, [bg_x - bg_width, 0])  # 左側の背景
-        screen.blit(bg_img, [bg_x, 0])  # 右側の背景
+        screen.blit(bg_img, [bg_x - bg_width, 0])  
+        screen.blit(bg_img, [bg_x, 0])  
 
-        # こうかとんの描画
         screen.blit(kk_img, kk_rct)
 
-        # 画面更新
         pg.display.update()
-        clock.tick(20)  # FPSを200に設定
+        clock.tick(200)
 
 if __name__ == "__main__":
     pg.init()
